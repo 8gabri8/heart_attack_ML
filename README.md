41596370d6efc9775c3b561849faa7ade51cd2bc
8gabri8 <gabri.dalai@gmail.com>
2024-09-21 12:46:36 +0200

    col selection

--- README.md
+++ README.md
@@ -1,26 +1,64 @@
 # heart_attack_ML
 
 The documentation must also include all data preparation, feature generation as well as cross-validation steps that you have used
+
+# Column/feature selection
+We wanted to reduce the number of features to avoid curse od dimentisonlity
+
+We decided to use cony "calculted vriables" because
+    - they ar ea few
+    - they summurce amny other vairables
+    - they are considered good indicatior of HT by the clinciacians/the guy wh made the sudy
+        cite"s. Thecommon focus of these variables is on health behaviors that are associated with a risk of illness or injury" https://www.cdc.gov/brfss/annual_data/2015/pdf/2015_Calculated_Variables_Version4_08_10_17-508c.pdf
+
+We decided to also add:
+    -
+
+We decied to remove the cols with more than % of NaN, as they will not be useful for classification
+
 .
 ## Exploratory data analysis 
-You should learn about your dataset - figure out which features are continuous, which
-ones are categorical, check if there are obvious relationships between the features, take a look at the distribution
-of each feature, and so on.
+- only use column with >thr of NaN
+- explore column 
+    - what columsn means (https://www.cdc.gov/brfss/annual_data/2015/pdf/codebook15_llcp.pdf)
+    - distirbution (zhuofu)
+    - percentage of NaN in each col
+    - other EDA steps (gabri)
 
-## Preprocessing
-- normalize/scaling
-- filter feature that are not Nan for at least N% of the observation
-- impute NaN values
-- remove correlated feautres
-- A feature may be important if it is highly correlated with the dependent variable (the thing being predicted).
-- combinaing features ex. polynomial
-- PCA
+- feautre selection
+    - retain only col that make sense for classification
+    - impute the missing values
+        - decide a startegy
+    - correlation analaysis
+        - remove cols that higly correlated
+    - 
+- preporcessing
+    - normalization ONLY on non-normalization
+    - ?) remove datapoitn so that we have balanced classes
+    - feture selection algo
+
+fearture enegerirng
+    - PCA
+    - combinaing features ex. polynomial
+
+
+
+# heck fucntions with the test of proposed by the TAs
 
 # Train
 - differt inital guesses of w
 - differt gamma for GD
 - use differnt optimizer Adam, momentum ...
 - use other losses
+- use other models: RF
+
+# testing locally on validation ONOTEHR is test on aicrowd
 
 # Plots
 - how the loss decrease in GD over the iterations
+- Try to find out which datapoints are wrongly classified and, if possible, why
+this is the case.
+# link to latex report
+
+# compettite evaluation
+[link](https://www.aicrowd.com/challenges/epfl-machine-learning-project-1)
---
+++ functions/implementations.py
@@ -0,0 +1,85 @@
+import numpy as np
+from my_functions import calculate_mse, compute_gradient
+from my_functions import calculate_mse, compute_gradient,batch_iter, compute_loss
+from my_functions import calculate_mae, compute_gradient
+
+def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
+    """Gradient Descent for Mean Squared Error (MSE).
+
+    Args:
+        y: shape=(N, ). The target values.
+        tx: shape=(N, D). The input data.
+        initial_w: shape=(D, ). The initial guess for the model parameters.
+        max_iters: a scalar denoting the total number of iterations of GD.
+        gamma: a scalar denoting the stepsize.
+
+    Returns:
+        A tuple (w, loss) where:
+            - w: the final model parameters after gradient descent (shape=(D,)).
+            - loss: the final loss value after the last iteration (scalar).
+    """
+    # Initialize parameters
+    w = initial_w
+
+    for n_iter in range(max_iters):
+
+        # Compute loss and gradient
+        grad, err = compute_gradient(y, tx, w, loss_type="mse")
+
+        # Update weights
+        w = w - gamma * grad
+        
+        # Calculate loss (MSE)
+        loss = calculate_mse(err)
+
+        print(
+            "GD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter + 1, ti=max_iters, l=loss, w0=w[0], w1=w[1]
+            )
+        )
+
+    return w, loss  # Return the final weights and loss
+
+def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
+    """The Stochastic Gradient Descent algorithm (SGD) for linear model.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N, D). Input data.
+        initial_w: shape=(D, ). The initial guess for the model parameters.
+        max_iters: a scalar denoting the total number of iterations of SGD.
+        gamma: a scalar denoting the stepsize.
+
+    Returns:
+        A tuple (w, loss) where:
+            - w: the final model parameters after stochastic gradient descent (shape=(D,)).
+            - loss: the final loss value after the last iteration (scalar).
+    """
+    # Initialize parameters
+    w = initial_w
+
+    # Force standard mini-batch-size 1 (sample just one datapoint).
+    batch_size = 1
+
+    # Set loss type to mse
+    loss_type = "mse"
+
+    for n_iter in range(max_iters):
+        
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size=batch_size, num_batches=1):
+            # Compute stochastic gradient and loss
+            grad, _ = compute_gradient(y_batch, tx_batch, w, loss_type)
+            # Update w through the stochastic gradient update
+            w = w - gamma * grad
+            
+            # Calculate loss for the current weights
+            loss = compute_loss(y_batch, tx_batch, w, loss_type)
+
+        print(
+            "SGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter + 1, ti=max_iters, l=loss, w0=w[0], w1=w[1]
+            )
+        )
+
+    return w, loss  # Return the final weights and loss
+
